2a199f653adf998caecf6017ae61f59dc170c322
Andrei250 <dumi.andrei0808@gmail.com>
2022-12-19 14:59:21 +0200

    Added feature layer

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -10,29 +10,36 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import GeoJSONLayer from '@arcgis/core/layers/GeoJSONLayer';
+import esriConfig from "@arcgis/core/config";
+import { firebaseAuth } from '../../firebase';
+import { useNavigate } from 'react-router-dom';
 
 function BucharestMap() {
     let
         map: Map,
         view: MapView,
-        zoom = 10,
+        zoom = 14,
         center: Array<number> = [26.096306, 44.439663],
-        basemap = "streets-vector",
+        basemap = "streets-navigation-vector",
         graphicsLayer: GraphicsLayer,
         pointCoords: number[] = [26.096306, 44.439663],
         pointGraphic: Graphic;
 
     const mapElement = useRef(null);
+    const navigate = useNavigate();
 
     useEffect(() => {
         map = new Map({
             basemap: basemap
         });
 
-        // esriConfig.apiKey = "AAPK8c1c645b82994e9f8c82acdc57febc098RwiOiGQUDWfauG-FNzYWqfJLlmIh01wSbF7fzHKvYZ7xQ3ygTe3yRG2R720qK8I";
+        esriConfig.apiKey = "AAPK8c1c645b82994e9f8c82acdc57febc098RwiOiGQUDWfauG-FNzYWqfJLlmIh01wSbF7fzHKvYZ7xQ3ygTe3yRG2R720qK8I";
 
         addFeatureLayers();
-        addPoint(pointCoords[1], pointCoords[0]);
+        // addGeoJSONLayer("https://services-eu1.arcgis.com/zci5bUiJ8olAal7N/arcgis/rest/services/OSM_Tourism_EU/FeatureServer/0/query?where=1%3D1&outFields=addr_street,attraction,board_type,building,description,information,name,opening_hours,operator,phone,tourism,zoo,osm_id2,website&outSR=4326&f=json");
+        // addPoint(pointCoords[1], pointCoords[0]);
+
 
         view = new MapView({
             map: map,
@@ -42,9 +49,24 @@ function BucharestMap() {
         });
 
 
+
+
     });
 
+    const addGeoJSONLayer = (url: string) => {
+        const layer: __esri.GeoJSONLayer = new GeoJSONLayer({
+            url: url
+        });
+
+        map.add(layer);
+    }
+
     const addFeatureLayers = () => {
+        const popupTrailheads = {
+            "title": "Trailhead",
+            "content": "{name}"
+          }
+
         const trailheadsLayer: __esri.FeatureLayer = new FeatureLayer({
             url:
                 "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"
@@ -71,10 +93,13 @@ function BucharestMap() {
 
         // Tourist layer
         const touristAttractionsLayer: __esri.FeatureLayer = new FeatureLayer({
-            url: "https://services.arcgis.com/.../FeatureServer/0"
+            url: "https://services-eu1.arcgis.com/zci5bUiJ8olAal7N/arcgis/rest/services/OSM_Tourism_EU/FeatureServer/0",
+            popupTemplate: popupTrailheads
         });
 
         map.add(touristAttractionsLayer);
+
+        touristAttractionsLayer.definitionExpression = "tourism = 'attraction' or tourism = 'zoo' or tourism = 'information' or tourism = 'artwork'";
     }
 
     const addPoint = (lat: number, lng: number) => {
@@ -105,6 +130,16 @@ function BucharestMap() {
         graphicsLayer.add(pointGraphic);
     }
 
+    const logout = (e) => {
+        e.preventDefault();
+
+        firebaseAuth.signOut().then(function () {
+            navigate('/auth', { replace: true });
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }
+
     return (
         <>
             <Navbar bg="light" expand="lg">
@@ -112,18 +147,18 @@ function BucharestMap() {
                     <Navbar.Brand ><Nav.Link href="/app">FeetForYou</Nav.Link></Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
+                        <Nav className="me-auto w-100">
                             <Nav.Link href="#home">Home</Nav.Link>
                             <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown title="User" id="basic-nav-dropdown" className={'ms-auto'} >
+                                {/* <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.2">
                                     Another action
                                 </NavDropdown.Item>
                                 <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
+                                <NavDropdown.Divider /> */}
+                                <NavDropdown.Item onClick={logout}>
+                                    Logout
                                 </NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
